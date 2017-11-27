7cc8560fae42cab365ea59902dae123bf3bb26d8
like0820 <33882742+like0820@users.noreply.github.com>
2017-11-27 21:44:05 +0900

    asd
    asd

--- src/MainView.java
+++ src/MainView.java
@@ -1,47 +1,10 @@
 
 public class MainView {
-public static void main(String[] args) {
-<<<<<<< HEAD
-	String str = "asdaseek";
-	System.out.println(str);
-=======
+	public static void main(String[] args) {
 
-	String str = "eek";
-	String name = "sungwon";
-	System.out.println(name);
+		String str = "eek";
+		String name = "sungwon";
+		System.out.println(name);
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	String str1 = "minssoJJAng";
-	int i = 0;
-	
-	
-
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	int sys = 465213;
-	System.out.println(sys);
-	
-	
->>>>>>> upstream/master
-}
+	}
 }
