92181d29d32bb36c1d760f8695d74c16c0a53b9c
dataartist93 <33890638+dataartist93@users.noreply.github.com>
2017-11-29 16:36:01 +0900

    Revert "CusDAO-3"
    This reverts commit f17a6f025a6ad8dcc5ed1d55e3fc62c1c39b94a7.

--- src/com/nol2/model/dao/CusDAO.java
+++ src/com/nol2/model/dao/CusDAO.java
@@ -11,15 +11,17 @@ import java.util.Properties;
 
 import com.nol2.model.vo.CusVO;
 
+
+
 public class CusDAO {
 
 	Connection conn;
 	PreparedStatement pstmt;
 	ResultSet rs;
-	Properties pro;
-
-	public CusDAO() {
-		try {
+    Properties pro;
+    
+    public CusDAO() {
+    	try {
 			pro = new Properties();
 			pro.load(new FileReader("conn/conn.properties"));
 
@@ -27,18 +29,16 @@ public class CusDAO {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-	}
-
-	private void connect() {
-		try {
+    }
+    private void connect() {
+    	try {
 			conn = DriverManager.getConnection(pro.getProperty("url"), pro);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-	}
-
-	private void disconnect() {
-		try {
+    }
+    private void disconnect() {
+    	try {
 			if (rs != null)
 				rs.close();
 			if (pstmt != null)
@@ -48,168 +48,101 @@ public class CusDAO {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-	}
-
-	// insert
-	public boolean insert(CusVO vo) {
-		connect();
-		try {
-			String sql = "insert into customer values(?,?,?,?,?,?,?,?,?,?)";
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setInt(1, vo.getCusno());
-			pstmt.setInt(2, vo.getEventno());
-			pstmt.setString(3, vo.getID());
-			pstmt.setString(4, vo.getPassword());
-			pstmt.setInt(5, vo.getJumin1());
-			pstmt.setInt(6, vo.getJumin2());
-			pstmt.setString(7, vo.getName());
-			pstmt.setString(8, vo.getTel());
-			pstmt.setString(9, vo.getAddr());
-			pstmt.setString(10, vo.getPerinfo());
+    }
+    public boolean insert(CusVO vo) {   
+    	connect();
+    	try {
+			String sql="insert into customer values(?,?,?,?,?,?,?,?,?,?)";
+			pstmt=conn.prepareStatement(sql);
+			pstmt.setInt(1,vo.getCusno());
+			pstmt.setInt(2,vo.getEventno());
+			pstmt.setString(3,vo.getID());
+			pstmt.setString(4,vo.getPassword());
+			pstmt.setInt(4,vo.getJumin1());
+			pstmt.setInt(5,vo.getJumin2());
+			pstmt.setString(6,vo.getName());
+			pstmt.setInt(7,vo.getTel());
+			pstmt.setString(8,vo.getAddr());
+			pstmt.setString(9,vo.getPerinfo());
 			pstmt.executeUpdate();
-
+			
 			return true;
 		} catch (SQLException e) {
 			e.printStackTrace();
-		} finally {
-			disconnect();
+		}finally {
+			disconnect();			
 		}
-		return false;
-	}
-
-	// delete
-	public boolean delete(String ID, String Password, int Jumin1, int Jumin2) {
-		connect();
-		try {
-			String sql = "delete from customer where ID=? and Password=? and"
-					+ "jumin1=? and jumin2=? ";
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, ID);
-			pstmt.setString(2, Password);
-			pstmt.setInt(3, Jumin1);
-			pstmt.setInt(4, Jumin2);
-			int t = pstmt.executeUpdate();
-			if (t == 1)
+    	return false;
+    }//insert
+    
+    public boolean delete(String delID) {
+    	connect();
+    	try {
+			String sql="delete from customer where ID=?";
+			pstmt=conn.prepareStatement(sql);
+			pstmt.setString(1,delID);
+			int t=pstmt.executeUpdate();
+			if(t==1)
 				return true;
 		} catch (SQLException e) {
 			e.printStackTrace();
-		} finally {
-			disconnect();
+		}finally {
+			disconnect();			
 		}
-		return false;
-	}
-
-	// update
-	public boolean update(CusVO vo) {
-		connect();
-		try {
-			String sql = "update customer set Password=?,Tel=?,addr=? where ID=?";
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, vo.getPassword());
-			pstmt.setString(2, vo.getTel());
-			pstmt.setString(3, vo.getAddr());
-			int t = pstmt.executeUpdate();
-			if (t == 1)
+    	return false;
+    }//delete
+    
+    public boolean update(CusVO vo) {
+    	connect();
+    	try {
+			String sql="update customer set Password=?,Tel=?,addr=? where ID=?";
+			pstmt=conn.prepareStatement(sql);
+			pstmt.setString(1,vo.getPassword());
+			pstmt.setInt(2,vo.getTel());
+			pstmt.setString(3,vo.getAddr());
+			int t= pstmt.executeUpdate();
+			if(t==1)
 				return true;
 		} catch (SQLException e) {
 			e.printStackTrace();
-		} finally {
-			disconnect();
+		}finally {
+			disconnect();			
 		}
-		return false;
-	}
-
-	// select
-	public CusVO select(String upID) {
-		connect();
-		CusVO cusvo = null;
-		try {
-			String sql = "select cusno,eventno,ID,Password,Jumin1,Jumin2,Name,Tel,addr,perinfo from customer where ID=?";
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, upID);
-			rs = pstmt.executeQuery();
-			if (rs.next()) {
-				cusvo = new CusVO();
-				cusvo.setCusno(rs.getInt("cusno"));
-				cusvo.setEventno(rs.getInt("eventno"));
-				cusvo.setID(rs.getString("ID"));
-				cusvo.setPassword(rs.getString("Password"));
-				cusvo.setJumin1(rs.getInt("Jumin1"));
-				cusvo.setJumin2(rs.getInt("Jumin2"));
-				cusvo.setName(rs.getString("Name"));
-				cusvo.setTel(rs.getString("Tel"));
-				cusvo.setAddr(rs.getString("addr"));
-				cusvo.setPerinfo(rs.getString("perinfo"));
+    	return false;
+    }//update
+    
+    public CusVO select(String upID) {
+    	connect();
+    	CusVO cusvo=null;
+    	try {
+			String sql="select cusno,eventno,ID,Password,Jumin1,Jumin2,Name,Tel,addr,perinfo from customer where ID=?";
+			pstmt=conn.prepareStatement(sql);
+			pstmt.setString(1,upID);
+			rs=pstmt.executeQuery();
+			if(rs.next()) {
+			 cusvo=new CusVO();
+			 cusvo.setCusno(rs.getInt("cusno"));
+			 cusvo.setEventno(rs.getInt("eventno"));
+			 cusvo.setID(rs.getString("ID"));
+			 cusvo.setPassword(rs.getString("Password"));
+			 cusvo.setJumin1(rs.getInt("Jumin1"));
+			 cusvo.setJumin2(rs.getInt("Jumin2"));
+			 cusvo.setName(rs.getString("Name"));
+			 cusvo.setTel(rs.getInt("Tel"));
+			 cusvo.setAddr(rs.getString("addr"));
+			 cusvo.setPerinfo(rs.getString("perinfo"));
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
-		} finally {
-			disconnect();
+		}finally {
+			disconnect();			
 		}
-		return cusvo;
-	}
+    	return cusvo;
+    }//select
+   
+    public void selectAll() {
+    	
+    }
 
-	// 로그인
-	public boolean selectLogin(String ID, String Password) {
-		connect();
-		try {
-			String sql = "select count(*) from customer where ID=? and Password=?";
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, ID);
-			pstmt.setString(2, Password);
-			rs = pstmt.executeQuery();
-			rs.next();
-			int count = rs.getInt(1);
-			if (count == 1) {
-				return true;// 로그인 성공
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			disconnect();
-		}
-		return false;// 로그인 실패
-	}
-
-	// 아이디 찾기
-	public boolean findID(String Name, int Jumin1, int Jumin2) {
-		connect();
-		try {
-			String sql = "select ID from customer where Name=? and Jumin1=? and Jumin2=?";
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, Name);
-			pstmt.setInt(2, Jumin1);
-			pstmt.setInt(3, Jumin2);
-			rs = pstmt.executeQuery();
-			rs.next();
-			return true;
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			disconnect();
-		}
-		return false;
-	}
-
-	// 비번 찾기
-	public boolean findPass(String ID, String Tel, int Jumin1, int Jumin2) {
-		connect();
-		try {
-			String sql = "select Password from customer where ID=? and Tel=? and Jumin1=? and Jumin2=?";
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, ID);
-			pstmt.setString(2, Tel);
-			pstmt.setInt(3, Jumin1);
-			pstmt.setInt(4, Jumin2);
-			rs = pstmt.executeQuery();
-			rs.next();
-			return true;
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			disconnect();
-		}
-		return false;
-	}
-
-}// CusDAO
+  
+}//CusDAO
--- src/com/nol2/model/vo/CusVO.java
+++ src/com/nol2/model/vo/CusVO.java
@@ -1,121 +1,123 @@
 package com.nol2.model.vo;
 
 public class CusVO {
+	
+  private int cusno;
+  private int eventno;
+  private String ID;
+  private String Password;
+  private int Jumin1;
+  private int Jumin2;
+  private String Name;
+  private int Tel;
+  private String addr;
+  private String perinfo;
+  
+public CusVO() {
+	
+}
 
-	private int cusno;
-	private int eventno;
-	private String ID;
-	private String Password;
-	private int Jumin1;
-	private int Jumin2;
-	private String Name;
-	private String Tel;
-	private String addr;
-	private String perinfo;
+public CusVO(int cusno,int eventno, String ID, String Password, int Jumin1, int Jumin2, String Name, int Tel,
+		String addr, String perinfo) {
+	super();
+	this.cusno=cusno;
+	this.eventno = eventno;
+	this.ID = ID;
+	this.Password = Password;
+	this.Jumin1 = Jumin1;
+	this.Jumin2 = Jumin2;
+	this.Name = Name;
+	this.Tel = Tel;
+	this.addr = addr;
+	this.perinfo = perinfo;
+}
 
-	public CusVO() {
+public String toString() {
+	return "CusVO [cusno=" + cusno + ", eventno=" + eventno + ", ID=" + ID + ", Password=" + Password + ", Jumin1="
+			+ Jumin1 + ", Jumin2=" + Jumin2 + ", Name=" + Name + ", Tel=" + Tel + ", addr=" + addr + ", perinfo="
+			+ perinfo + "]";
+}
 
-	}
+public int getCusno() {
+	return cusno;
+}
 
-	public CusVO(int cusno, int eventno, String ID, String Password, int Jumin1, int Jumin2, String Name, String Tel,
-			String addr, String perinfo) {
-		super();
-		this.cusno = cusno;
-		this.eventno = eventno;
-		this.ID = ID;
-		this.Password = Password;
-		this.Jumin1 = Jumin1;
-		this.Jumin2 = Jumin2;
-		this.Name = Name;
-		this.Tel = Tel;
-		this.addr = addr;
-		this.perinfo = perinfo;
-	}
+public void setCusno(int cusno) {
+	this.cusno = cusno;
+}
 
-	public String toString() {
-		return "CusVO [cusno=" + cusno + ", eventno=" + eventno + ", ID=" + ID + ", Password=" + Password + ", Jumin1="
-				+ Jumin1 + ", Jumin2=" + Jumin2 + ", Name=" + Name + ", Tel=" + Tel + ", addr=" + addr + ", perinfo="
-				+ perinfo + "]";
-	}
+public int getEventno() {
+	return eventno;
+}
 
-	public int getCusno() {
-		return cusno;
-	}
+public void setEventno(int eventno) {
+	this.eventno = eventno;
+}
 
-	public void setCusno(int cusno) {
-		this.cusno = cusno;
-	}
+public String getID() {
+	return ID;
+}
 
-	public int getEventno() {
-		return eventno;
-	}
+public void setID(String ID) {
+	ID = ID;
+}
 
-	public void setEventno(int eventno) {
-		this.eventno = eventno;
-	}
+public String getPassword() {
+	return Password;
+}
 
-	public String getID() {
-		return ID;
-	}
+public void setPassword(String Password) {
+	Password = Password;
+}
 
-	public void setID(String ID) {
-		ID = ID;
-	}
+public int getJumin1() {
+	return Jumin1;
+}
 
-	public String getPassword() {
-		return Password;
-	}
+public void setJumin1(int Jumin1) {
+	Jumin1 = Jumin1;
+}
 
-	public void setPassword(String Password) {
-		Password = Password;
-	}
+public int getJumin2() {
+	return Jumin2;
+}
 
-	public int getJumin1() {
-		return Jumin1;
-	}
+public void setJumin2(int Jumin2) {
+	Jumin2 = Jumin2;
+}
 
-	public void setJumin1(int Jumin1) {
-		Jumin1 = Jumin1;
-	}
+public String getName() {
+	return Name;
+}
 
-	public int getJumin2() {
-		return Jumin2;
-	}
+public void setName(String Name) {
+	Name = Name;
+}
 
-	public void setJumin2(int Jumin2) {
-		Jumin2 = Jumin2;
-	}
+public int getTel() {
+	return Tel;
+}
 
-	public String getName() {
-		return Name;
-	}
+public void setTel(int Tel) {
+	Tel = Tel;
+}
 
-	public void setName(String Name) {
-		Name = Name;
-	}
+public String getAddr() {
+	return addr;
+}
 
-	public String getTel() {
-		return Tel;
-	}
+public void setAddr(String addr) {
+	this.addr = addr;
+}
 
-	public void setTel(String Tel) {
-		Tel = Tel;
-	}
+public String getPerinfo() {
+	return perinfo;
+}
 
-	public String getAddr() {
-		return addr;
-	}
+public void setPerinfo(String perinfo) {
+	this.perinfo = perinfo;
+}
 
-	public void setAddr(String addr) {
-		this.addr = addr;
-	}
 
-	public String getPerinfo() {
-		return perinfo;
-	}
-
-	public void setPerinfo(String perinfo) {
-		this.perinfo = perinfo;
-	}
-
+  
 }
