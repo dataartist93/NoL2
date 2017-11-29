f17a6f025a6ad8dcc5ed1d55e3fc62c1c39b94a7
songdajeong <ekwjd72@naver.com>
2017-11-29 16:19:50 +0900

    CusDAO-3
    2017-11-29

--- src/com/nol2/model/vo/CusVO.java
+++ src/com/nol2/model/vo/CusVO.java
@@ -1,123 +1,121 @@
 package com.nol2.model.vo;
 
 public class CusVO {
-	
-  private int cusno;
-  private int eventno;
-  private String ID;
-  private String Password;
-  private int Jumin1;
-  private int Jumin2;
-  private String Name;
-  private int Tel;
-  private String addr;
-  private String perinfo;
-  
-public CusVO() {
-	
-}
 
-public CusVO(int cusno,int eventno, String ID, String Password, int Jumin1, int Jumin2, String Name, int Tel,
-		String addr, String perinfo) {
-	super();
-	this.cusno=cusno;
-	this.eventno = eventno;
-	this.ID = ID;
-	this.Password = Password;
-	this.Jumin1 = Jumin1;
-	this.Jumin2 = Jumin2;
-	this.Name = Name;
-	this.Tel = Tel;
-	this.addr = addr;
-	this.perinfo = perinfo;
-}
+	private int cusno;
+	private int eventno;
+	private String ID;
+	private String Password;
+	private int Jumin1;
+	private int Jumin2;
+	private String Name;
+	private String Tel;
+	private String addr;
+	private String perinfo;
 
-public String toString() {
-	return "CusVO [cusno=" + cusno + ", eventno=" + eventno + ", ID=" + ID + ", Password=" + Password + ", Jumin1="
-			+ Jumin1 + ", Jumin2=" + Jumin2 + ", Name=" + Name + ", Tel=" + Tel + ", addr=" + addr + ", perinfo="
-			+ perinfo + "]";
-}
+	public CusVO() {
 
-public int getCusno() {
-	return cusno;
-}
+	}
 
-public void setCusno(int cusno) {
-	this.cusno = cusno;
-}
+	public CusVO(int cusno, int eventno, String ID, String Password, int Jumin1, int Jumin2, String Name, String Tel,
+			String addr, String perinfo) {
+		super();
+		this.cusno = cusno;
+		this.eventno = eventno;
+		this.ID = ID;
+		this.Password = Password;
+		this.Jumin1 = Jumin1;
+		this.Jumin2 = Jumin2;
+		this.Name = Name;
+		this.Tel = Tel;
+		this.addr = addr;
+		this.perinfo = perinfo;
+	}
 
-public int getEventno() {
-	return eventno;
-}
+	public String toString() {
+		return "CusVO [cusno=" + cusno + ", eventno=" + eventno + ", ID=" + ID + ", Password=" + Password + ", Jumin1="
+				+ Jumin1 + ", Jumin2=" + Jumin2 + ", Name=" + Name + ", Tel=" + Tel + ", addr=" + addr + ", perinfo="
+				+ perinfo + "]";
+	}
 
-public void setEventno(int eventno) {
-	this.eventno = eventno;
-}
+	public int getCusno() {
+		return cusno;
+	}
 
-public String getID() {
-	return ID;
-}
+	public void setCusno(int cusno) {
+		this.cusno = cusno;
+	}
 
-public void setID(String ID) {
-	ID = ID;
-}
+	public int getEventno() {
+		return eventno;
+	}
 
-public String getPassword() {
-	return Password;
-}
+	public void setEventno(int eventno) {
+		this.eventno = eventno;
+	}
 
-public void setPassword(String Password) {
-	Password = Password;
-}
+	public String getID() {
+		return ID;
+	}
 
-public int getJumin1() {
-	return Jumin1;
-}
+	public void setID(String ID) {
+		ID = ID;
+	}
 
-public void setJumin1(int Jumin1) {
-	Jumin1 = Jumin1;
-}
+	public String getPassword() {
+		return Password;
+	}
 
-public int getJumin2() {
-	return Jumin2;
-}
+	public void setPassword(String Password) {
+		Password = Password;
+	}
 
-public void setJumin2(int Jumin2) {
-	Jumin2 = Jumin2;
-}
+	public int getJumin1() {
+		return Jumin1;
+	}
 
-public String getName() {
-	return Name;
-}
+	public void setJumin1(int Jumin1) {
+		Jumin1 = Jumin1;
+	}
 
-public void setName(String Name) {
-	Name = Name;
-}
+	public int getJumin2() {
+		return Jumin2;
+	}
 
-public int getTel() {
-	return Tel;
-}
+	public void setJumin2(int Jumin2) {
+		Jumin2 = Jumin2;
+	}
 
-public void setTel(int Tel) {
-	Tel = Tel;
-}
+	public String getName() {
+		return Name;
+	}
 
-public String getAddr() {
-	return addr;
-}
+	public void setName(String Name) {
+		Name = Name;
+	}
 
-public void setAddr(String addr) {
-	this.addr = addr;
-}
+	public String getTel() {
+		return Tel;
+	}
 
-public String getPerinfo() {
-	return perinfo;
-}
+	public void setTel(String Tel) {
+		Tel = Tel;
+	}
 
-public void setPerinfo(String perinfo) {
-	this.perinfo = perinfo;
-}
+	public String getAddr() {
+		return addr;
+	}
 
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
 
-  
+	public String getPerinfo() {
+		return perinfo;
+	}
+
+	public void setPerinfo(String perinfo) {
+		this.perinfo = perinfo;
+	}
+
 }
