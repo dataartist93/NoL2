95240b01f811b4ce4a5aa91625f5b06d6f2d150c
mycraig <mycraig@naver.com>
2017-12-01 10:41:22 +0900

    FindPW_View 수정
    20171201 전화번호 입력란 수정

--- src/com/nol2/view/FindPW_View.java
+++ src/com/nol2/view/FindPW_View.java
@@ -9,10 +9,10 @@ import javax.swing.JTextField;
 
 public class FindPW_View extends JFrame {
 
-	public JTextField tf_id, tf_tel, tf_jumin1;
+	public JTextField tf_id, tf_tel1,tf_tel2,tf_tel3, tf_jumin1;
 	public JPasswordField pf_jumin2;
 	public JButton bt_confirm, bt_cancel;
-	public JLabel la_id, la_tel, la_jumin1,la_jumin2;
+	public JLabel la_id, la_tel,la_telhypen1,la_telhypen2, la_jumin1,la_jumin2;
 
 	public FindPW_View() {
 
@@ -21,7 +21,11 @@ public class FindPW_View extends JFrame {
 		tf_id = new JTextField();
 
 		la_tel = new JLabel("전화번호 :");
-		tf_tel = new JTextField();
+		tf_tel1 = new JTextField();
+		tf_tel2 = new JTextField();
+		tf_tel3 = new JTextField();
+		la_telhypen1 = new JLabel("-");
+		la_telhypen2 = new JLabel("-");
 		
 		la_jumin1 = new JLabel("주민등록번호 :");
 		tf_jumin1 = new JTextField();
@@ -31,24 +35,32 @@ public class FindPW_View extends JFrame {
 		bt_confirm = new JButton("확인");
 		bt_cancel = new JButton("취소");
 
-		tf_id.setBounds(125, 106, 177, 25);
+		tf_id.setBounds(125, 106, 193, 25);
 		la_id.setBounds(57, 106, 57, 25);
 
-		tf_tel.setBounds(125, 180, 177, 30);
+		tf_tel1.setBounds(122, 180, 50, 30);
+		tf_tel2.setBounds(194, 180, 50, 30);
+		tf_tel3.setBounds(268, 180, 50, 30);
 		la_tel.setBounds(43, 185, 71, 25);
+		la_telhypen1.setBounds(179, 180, 18, 30);
+		la_telhypen2.setBounds(252, 180, 18, 30);
 
 		la_jumin1.setBounds(14, 145, 100, 25);
-		tf_jumin1.setBounds(125, 143, 65, 25);
-		la_jumin2.setBounds(194, 144, 18, 30);
-		pf_jumin2.setBounds(215, 143, 87, 25);
+		tf_jumin1.setBounds(125, 143, 76, 25);
+		la_jumin2.setBounds(210, 144, 18, 30);
+		pf_jumin2.setBounds(231, 143, 87, 25);
 
-		bt_confirm.setBounds(30, 235, 123, 42);
-		bt_cancel.setBounds(179, 236, 123, 41);
+		bt_confirm.setBounds(43, 235, 123, 42);
+		bt_cancel.setBounds(195, 236, 123, 41);
 
 
 		getContentPane().setLayout(null);
 		getContentPane().add(tf_id);
-		getContentPane().add(tf_tel);
+		getContentPane().add(tf_tel1);
+		getContentPane().add(tf_tel2);
+		getContentPane().add(tf_tel3);
+		getContentPane().add(la_telhypen1);
+		getContentPane().add(la_telhypen2);
 		getContentPane().add(tf_jumin1);
 		getContentPane().add(pf_jumin2);
 		getContentPane().add(bt_confirm);
@@ -58,7 +70,7 @@ public class FindPW_View extends JFrame {
 		getContentPane().add(la_jumin1);
 		getContentPane().add(la_jumin2);
 
-		setBounds(350, 200, 350, 350);
+		setBounds(350, 200, 365, 356);
 		setVisible(true);
 		setResizable(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
